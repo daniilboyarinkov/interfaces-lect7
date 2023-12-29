064732c1b51f7a030ded49b0480c49b1af123e81
Daniel Boyarinkov <89917619+daniilboyarinkov@users.noreply.github.com>
2023-12-29 10:54:53 +0700

    Update mycode.cpp

--- src/mycode.cpp
+++ src/mycode.cpp
@@ -1,26 +1,38 @@
 #include "mycode.h"
 #include "QVector"
-#include "QString"
 
-bool match_from_mycode(string &s, int pos, string &sub) {
-    if (s.size()-pos < sub.size())
-        return false;
+QVector<int> boyerMooreSearch(string &text, string &pattern) {
+    int n = text.length();
+    int m = pattern.length();
 
-    for (int i = 0; i < sub.size(); i++) {
-        if (s[pos+i] != sub[i])
-            return false;
+    QVector<int> indexes;
+
+    if (m == 0) {
+        indexes.append(0); // Пустая подстрока всегда найдется
     }
-    return true;
-}
 
+    QVector<int> shift(256, m);
+    for (int i = 0; i < m - 1; i++) {
+        shift[pattern[i]] = m - 1 - i;
+    }
 
-QVector<int> my_substr_mycode(string &s, string &sub) {
-    QVector<int> inds;
-
-    for (int i = 0; i < s.size(); i++) {
-        if (match_from_mycode(s, i, sub)) {
-            inds.push_back(i);
+    int i = m - 1; // Индекс для прохода по тексту
+    int j = m - 1; // Индекс для прохода по подстроке
+    do {
+        if (text[i] == pattern[j]) {
+            if (j == 0) {
+                indexes.append(i); // Найдено полное совпадение
+                i--;
+                j = m - 1;
+            } else {
+                i--;
+                j--;
+            }
+        } else {
+            i += shift[text[i]] > m - j ? shift[text[i]] : m - j;
+            j = m - 1;
         }
-    }
-    return inds;
+    } while (i < n);
+
+    return indexes;
 }
